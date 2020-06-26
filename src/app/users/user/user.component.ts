eee8eec027df5d9760f0549b597529842ee86cd1
Firas Masmoudi <fim@wevioo.com>
2020-06-26 07:33:57 +0100

    jusqu'a etape1 done

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    const name = this.route.snapshot.paramMap.get('name');
+    this.user = {id: +id, name} ;
+
+    this.route.paramMap.subscribe(
+      (paramMap: ParamMap) => this.user = {
+        id: +paramMap.get('id'), 
+        name: paramMap.get('name')
+      }
+    );
   }
 
 }
