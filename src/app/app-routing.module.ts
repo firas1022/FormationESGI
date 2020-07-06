378b2c9e8b384081b5aadc637990c411b2cef8dd
Firas Masmoudi <fim@wevioo.com>
2020-07-06 16:17:45 +0100

    authGuard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,8 @@ import {ServersComponent} from './servers/servers.component';
 import {ServerComponent} from './servers/server/server.component';
 import {EditServerComponent} from './servers/edit-server/edit-server.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {AuthGuardService} from './auth-guard.service';
+import {UnauthorizedComponent} from './unauthorized/unauthorized.component';
 
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
@@ -22,12 +24,14 @@ const appRoutes: Routes = [
   {
     path: 'servers',
     component: ServersComponent,
+    canActivate: [AuthGuardService],
     children: [
       {path: ':id', component: ServerComponent},
       {path: ':id/edit', component: EditServerComponent}
     ]
   },
   {path: 'page-not-found', component: PageNotFoundComponent},
+  {path: 'unauthorised', component: UnauthorizedComponent},
   {path: '**', redirectTo: 'page-not-found'},
   // { path: 'servers/:id', component: ServerComponent },
 ];
---
+++ src/app/auth.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private loggedIn = false;
+
+  constructor() {
+  }
+
+  isAuthenticated() {
+    return this.loggedIn;
+  }
+
+  login() {
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+}
